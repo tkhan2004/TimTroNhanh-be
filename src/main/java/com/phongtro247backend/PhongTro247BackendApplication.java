fb04e6be5c5d5485ff74f014e1434ee22545aa48
khangnguyen <tcag24042004@gmail.com>
2025-07-17 23:35:14 +0700

    first commit

---
+++ src/main/java/com/phongtro247backend/PhongTro247BackendApplication.java
@@ -0,0 +1,13 @@
+package com.phongtro247backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhongTro247BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhongTro247BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phongtro247backend/PhongTro247BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phongtro247backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhongTro247BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
